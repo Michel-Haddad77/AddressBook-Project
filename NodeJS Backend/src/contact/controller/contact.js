adb6288de5f10160527212e6865a495f56642df0
Micho-PC <michelhdd27@gmail.com>
2022-06-24 23:31:49 +0300

    created get contacts API

--- NodeJS Backend/src/contact/controller/contact.js
+++ NodeJS Backend/src/contact/controller/contact.js
@@ -1,8 +1,9 @@
 const Contact = require("../../../models/Contact");
+const User = require("../../../models/User");
 
 async function addContact(req,res){
     try{
-        console.log(req.body);
+        //console.log(req.body);
 
         const {
             name,
@@ -16,6 +17,9 @@ async function addContact(req,res){
         //get user_id from params
         const user_id = req.query.id;
 
+        //check if sent user id is correct
+        const user = await User.findOne({_id: user_id});
+        if(!user) return res.status(400).send("Bad User id");
 
         //create new contact in db
         const contact = new Contact({
@@ -30,15 +34,38 @@ async function addContact(req,res){
 
         const added_contact = await contact.save();
 
+        //update the contact array in user
+        await User.findByIdAndUpdate(user_id,
+            {
+                $push: {
+                    contacts: contact._id
+                  }
+            });
+
         console.log('addContact =>', added_contact);
 
         return res.send(added_contact);
 
     }catch(error){
         console.log(error);
+        res.status(500).send(error);
+    }
+}
+
+async function getContacts(req,res){
+    try{
+        //get user_id from params
+        const user_id = req.query.id;
+
+        const contacts = await Contact.find({user_id: user_id});
+        return res.send(contacts);
+    }catch(error){
+        console.log(error);
+        res.status(500).send(error);
     }
 }
 
 module.exports = {
-    addContact
+    addContact,
+    getContacts
   };
--- NodeJS Backend/src/contact/index.js
+++ NodeJS Backend/src/contact/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { addContact } = require('./controller/contact');
+const { addContact, getContacts } = require('./controller/contact');
 
 const router = Router();
 
 router.post('/add_contact', addContact);
+router.post('/get_contacts', getContacts);
 
 module.exports = router;
