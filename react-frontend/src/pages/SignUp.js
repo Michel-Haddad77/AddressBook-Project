aa68a0ffc21dacc3b0468e50316609f5fad5593c
Micho-PC <michelhdd27@gmail.com>
2022-06-25 19:36:12 +0300

    created signup page with linking

---
+++ react-frontend/src/pages/SignUp.js
@@ -0,0 +1,70 @@
+import Button from "../Components/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+function SignUp(){
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    function signUp(){
+        let data = {
+            "name":name,
+            "email":email,
+            "password":password
+        }
+    
+        //linking with sign up api
+        axios({
+            method: 'post',
+            url: 'http://localhost:8080/api/user/register',
+            data: data,
+        })
+        .then(function (response) {
+            console.log(response.data);
+            alert(response.data.name + " has successfully registered! \nPlease login to proceed");
+            //navigate to login page
+            navigate("/");
+        })
+        .catch(function (error){
+            console.log(error);
+        })
+    }
+
+    return(
+        <form className="login-container"onSubmit={(e) => {
+            e.preventDefault();
+            signUp();
+        }}>
+            <h3 className="login-h3">Sign Up</h3>
+            <input 
+                type="text" 
+                placeholder="Name"
+                onChange={(e) => {
+                    setName(e.target.value);
+                }}    
+            ></input><br/>
+            <input 
+                type="text" 
+                placeholder="Email address"
+                onChange={(e) => {
+                    setEmail(e.target.value);
+                }}
+            /><br/>
+            <input 
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => {
+                    setPassword(e.target.value);
+                }}
+            /><br/>
+            <Button type="submit" text="Sign Up" />
+            <h4>Already have an account? <Link to="/">Login</Link></h4>
+        </form>
+    );
+};
+
+export default SignUp;
