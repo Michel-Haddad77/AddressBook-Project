abc21ccba3061465b6623fead9d69e8dd356d7c5
Micho-PC <michelhdd27@gmail.com>
2022-06-26 15:13:01 +0300

    created working map component using leaflet

---
+++ react-frontend/src/Components/Map.js
@@ -0,0 +1,16 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from "leaflet";
+
+function Map() {
+  return (
+    <MapContainer center={[45.4, -75.7]} zoom={12}scrollWheelZoom={false}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[45.4, -75.7]}/>
+    </MapContainer>
+  );
+}
+
+export default Map;
